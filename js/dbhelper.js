eb66c9f72597c4f67c53396cca3afe505f6bbc69
Panos Panagiotidis <panpan291265@gmail.com>
2018-02-28 21:33:04 +0200

    refactor: Change restaurants json data url

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,10 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    // const port = 8000;
+    // const dbUrl = `http://localhost:${port}/data/restaurants.json`;
+    const dbUrl = '/data/restaurants.json';
+    return dbUrl;
   }
 
   /**
