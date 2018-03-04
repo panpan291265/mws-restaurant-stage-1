669940884d97f59d3cfb4e7496fa4fc36517f4e1
Panos Panagiotidis <panpan291265@gmail.com>
2018-03-04 16:01:12 +0200

    feat: Add unavailable page
    Add unavailable.html page for not available
    resources on offline use.
    Reformat (style only) html pages.

--- serviceWorker.js
+++ serviceWorker.js
@@ -7,32 +7,32 @@ self.addEventListener('install', event => {
 
     event.waitUntil(
         caches.open(cacheName)
-            .then(function (cache) {
-                // Cache main application resources
-                let requests = [
-                    'index.html', 'restaurant.html',
-                    'data/restaurants.json',
-                    'img/restaurant-128.png',
-                    'img/restaurant-256.png',
-                    'img/restaurant.png',
-                    'css/styles.css', 'css/styles.min.css',
-                    'js/urlhelper.js', 'js/urlhelper.min.js',
-                    'js/dbhelper.js', 'js/dbhelper.min.js',
-                    'js/main.js', 'js/main.min.js',
-                    'js/restaurant_info.js', 'js/restaurant_info.min.js'
-                ];
+        .then(function (cache) {
+            // Cache main application resources
+            let requests = [
+                'index.html', 'restaurant.html', 'unavailable.html',
+                'data/restaurants.json',
+                'img/restaurant-128.png',
+                'img/restaurant-256.png',
+                'img/restaurant.png',
+                'css/styles.css', 'css/styles.min.css',
+                'js/urlhelper.js', 'js/urlhelper.min.js',
+                'js/dbhelper.js', 'js/dbhelper.min.js',
+                'js/main.js', 'js/main.min.js',
+                'js/restaurant_info.js', 'js/restaurant_info.min.js'
+            ];
 
-                const dataImgSuffs = ['', '-200', '-300', '-400', '-500', '-600'];
-                for (let i = 1; i <= 10; i++) {
-                    // Cache data images preactivelly
-                    dataImgSuffs.forEach(imgSuffix => requests.push(`img/${i}${imgSuffix}.jpg`));
+            const dataImgSuffs = ['', '-200', '-300', '-400', '-500', '-600'];
+            for (let i = 1; i <= 10; i++) {
+                // Cache data images preactivelly
+                dataImgSuffs.forEach(imgSuffix => requests.push(`img/${i}${imgSuffix}.jpg`));
 
-                    // Cache restaurant sites preactivelly
-                    // requests.push(`restaurant.html?id=${i}`);
-                }
-                return cache.addAll(requests).then(() => self.skipWaiting());
-            })
-            .catch(err => console.error(err))
+                // Cache restaurant sites preactivelly
+                // requests.push(`restaurant.html?id=${i}`);
+            }
+            return cache.addAll(requests).then(() => self.skipWaiting());
+        })
+        .catch(err => console.error(err))
     );
 });
 
@@ -46,18 +46,27 @@ self.addEventListener('fetch', event => {
 
     const responsePromise = new Promise((resolve, reject) => {
         caches.open(cacheName).then(cache => {
-            let cacheKey = event.request;
+            let cacheKey = event.request.url;
             cache.match(cacheKey).then(cachedResponse => {
                 if (cachedResponse)
                     return resolve(cachedResponse);
                 fetch(event.request).then(networkResponse => {
-                    // console.log(`Adding new cache item: '${cacheKey}'`);
-                    cache.put(cacheKey, networkResponse.clone());
-                    return resolve(networkResponse);
-                });
+                        // console.log(`Adding new cache item: '${cacheKey}'`);
+                        cache.put(cacheKey, networkResponse.clone());
+                        return resolve(networkResponse);
+                    })
+                    .catch(err => {
+                        if (event.request.url.includes('restaurant.html?id=')) {
+                            cache.match('unavailable.html').then(unavailableResponse => {
+                                return resolve(unavailableResponse);
+                            });
+                        } else {
+                            return reject(err);
+                        }
+
+                    });
             });
-        })
-        .catch(err => reject(err));
+        });
     });
 
     event.respondWith(responsePromise);
